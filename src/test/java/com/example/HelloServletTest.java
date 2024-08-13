87315493daf6737205480d50722c4b7480be6842
lily4499 <konissil@gmail.com>
2024-08-12 22:33:41 -0400

    first commit

---
+++ src/test/java/com/example/HelloServletTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HelloServletTest {
+    
+    @Test
+    public void testServlet() {
+        assertTrue(true); // Simple test to validate setup
+    }
+}
